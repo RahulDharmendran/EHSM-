3e458c346db1fd420af6f365f41feeb1c924dfef
RahulDharmendran <rahuldharmendran1912@gmail.com>
2025-12-17 10:22:19 +0530

    feat: Implement initial UI views and controllers for Risk and Incident List management.

--- webapp/controller/IncidentList.controller.js
+++ webapp/controller/IncidentList.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "sap/ui/core/Fragment"
-], function (Controller, Filter, FilterOperator, Fragment) {
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, Filter, FilterOperator, Fragment, JSONModel) {
     "use strict";
 
     return Controller.extend("ehsmportal.controller.IncidentList", {
@@ -14,14 +15,40 @@ sap.ui.define([
 
         _onObjectMatched: function (oEvent) {
             this._sEmployeeId = oEvent.getParameter("arguments").employeeId;
+            this._loadData();
+        },
 
-            // Filter Table by EmployeeId
+        _loadData: function () {
             var oTable = this.byId("incidentTable");
-            var oBinding = oTable.getBinding("items");
-            if (oBinding) {
-                var oFilter = new Filter("EmployeeId", FilterOperator.EQ, this._sEmployeeId);
-                oBinding.filter([oFilter]);
-            }
+            oTable.setBusy(true);
+
+            var oModel = this.getOwnerComponent().getModel();
+            var aFilters = [new Filter("EmployeeId", FilterOperator.EQ, this._sEmployeeId)];
+
+            oModel.read("/ZRD_EHSM_INCIDENTSet", {
+                filters: aFilters,
+                success: function (oData) {
+                    var aResults = oData.results || [];
+
+                    // Manually fix dates for JSON Model as it doesn't parse them automatically like ODataModel
+                    aResults.forEach(function (oItem) {
+                        if (oItem.IncidentDate) { oItem.IncidentDate = new Date(oItem.IncidentDate); }
+                        if (oItem.CompletionDate && oItem.CompletionDate.indexOf("0000") === -1) {
+                            oItem.CompletionDate = new Date(oItem.CompletionDate);
+                        } else {
+                            oItem.CompletionDate = null;
+                        }
+                    });
+
+                    var oJSONModel = new JSONModel({ results: aResults });
+                    this.getView().setModel(oJSONModel);
+                    oTable.setBusy(false);
+                }.bind(this),
+                error: function (oError) {
+                    oTable.setBusy(false);
+                    sap.m.MessageToast.show("Failed to load incidents.");
+                }
+            });
         },
 
         onNavBack: function () {
--- webapp/controller/RiskList.controller.js
+++ webapp/controller/RiskList.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "sap/ui/core/Fragment"
-], function (Controller, Filter, FilterOperator, Fragment) {
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, Filter, FilterOperator, Fragment, JSONModel) {
     "use strict";
 
     return Controller.extend("ehsmportal.controller.RiskList", {
@@ -14,14 +15,35 @@ sap.ui.define([
 
         _onObjectMatched: function (oEvent) {
             this._sEmployeeId = oEvent.getParameter("arguments").employeeId;
+            this._loadData();
+        },
 
-            // Filter Table by EmployeeId
+        _loadData: function () {
             var oTable = this.byId("riskTable");
-            var oBinding = oTable.getBinding("items");
-            if (oBinding) {
-                var oFilter = new Filter("EmployeeId", FilterOperator.EQ, this._sEmployeeId);
-                oBinding.filter([oFilter]);
-            }
+            oTable.setBusy(true);
+
+            var oModel = this.getOwnerComponent().getModel();
+            var aFilters = [new Filter("EmployeeId", FilterOperator.EQ, this._sEmployeeId)];
+
+            oModel.read("/ZRD_EHSH_RISKSet", {
+                filters: aFilters,
+                success: function (oData) {
+                    var aResults = oData.results || [];
+
+                    // Manually fix dates for JSON Model
+                    aResults.forEach(function (oItem) {
+                        if (oItem.RiskIdentificationDate) { oItem.RiskIdentificationDate = new Date(oItem.RiskIdentificationDate); }
+                    });
+
+                    var oJSONModel = new JSONModel({ results: aResults });
+                    this.getView().setModel(oJSONModel);
+                    oTable.setBusy(false);
+                }.bind(this),
+                error: function (oError) {
+                    oTable.setBusy(false);
+                    sap.m.MessageToast.show("Failed to load risks.");
+                }
+            });
         },
 
         onNavBack: function () {
